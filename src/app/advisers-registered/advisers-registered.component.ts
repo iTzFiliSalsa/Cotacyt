a95aa60274b8c7b8caeb905b79e736886dece34d
Santia0098 <56708822+Santia0098@users.noreply.github.com>
2020-09-18 00:41:46 -0500

    descarga pdf por cada proyecto del asesor

--- src/app/advisers-registered/advisers-registered.component.ts
+++ src/app/advisers-registered/advisers-registered.component.ts
@@ -111,72 +111,86 @@ export class AdvisersRegisteredComponent implements OnInit {
         this._utilService._loading = false;
       });
   }
-  saveAsPdf(asesor: Asesores){
+  saveAsPdf(asesor: any){
     this.asesorActual = asesor;
     console.log(this.asesorActual);
   switch(this.asesorActual.id_sedes){
     case '1':
+      for (let i = 0; i < asesor.proyectos.length; i++) {
       const doc = new jsPDF();
       doc.addImage('assets/image/certificadoAsesorMante.jpg', 'jpg', 0, 0, 210, 300);
-      doc.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 65, 185);
-      doc.text(this.asesorActual.proyecto, 80, 225);
+      doc.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 70, 185);
+      doc.text(asesor.proyectos[i], 90, 225);
       doc.setFontSize(16);
       doc.setFont('Helvetica');
-      doc.save("constanciaAsesor.pdf");
+      doc.save("constancia Asesor "+this.asesorActual.nombres+" Proyecto "+asesor.proyectos[i]+".pdf");
+      }
     break;
     case '2':
+      for (let i = 0; i < asesor.proyectos.length; i++) {
       const doc1 = new jsPDF();
       doc1.addImage('assets/image/certificadoAsesorReynosa.jpg', 'jpg', 0, 0, 210, 300);
-      doc.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 65, 185);
-      doc.text(this.asesorActual.proyecto, 80, 225);
+      doc1.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 70, 185);
+      doc1.text(asesor.proyectos[i], 90, 225);
       doc1.setFontSize(16);
       doc1.setFont('Helvetica');
-      doc1.save("constanciaAsesor.pdf");
+      doc1.save("constancia Asesor "+this.asesorActual.nombres+" Proyecto "+asesor.proyectos[i]+".pdf");
+      }
     break;
     case '3':
+      for (let i = 0; i < asesor.proyectos.length; i++) {
       const doc2 = new jsPDF();
       doc2.addImage('assets/image/certificadoAsesorMatamoros.jpg', 'jpg', 0, 0, 210, 300);
-      doc.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 65, 185);
-      doc.text(this.asesorActual.proyecto, 80, 225);
+      doc2.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 70, 185);
+      doc2.text(asesor.proyectos[i], 90, 225);
       doc2.setFontSize(16);
       doc2.setFont('Helvetica');
-      doc2.save("constanciaAsesor.pdf");
+      doc2.save("constancia Asesor "+this.asesorActual.nombres+" Proyecto "+asesor.proyectos[i]+".pdf");
+      }
     break;
     case '4':
+      for (let i = 0; i < asesor.proyectos.length; i++) {
       const doc3 = new jsPDF();
       doc3.addImage('assets/image/certificadoAsesorMadero.jpg', 'jpg', 0, 0, 210, 300);
-      doc.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 65, 185);
-      doc.text(this.asesorActual.proyecto, 80, 225);
+      doc3.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 70, 185);
+      doc3.text(asesor.proyectos[i], 90, 225);
       doc3.setFontSize(16);
       doc3.setFont('Helvetica');
-      doc3.save("constanciaAsesor.pdf");
+      doc3.save("constancia Asesor "+this.asesorActual.nombres+" Proyecto "+asesor.proyectos[i]+".pdf");
+      }
     break;
     case '5':
+      for (let i = 0; i < asesor.proyectos.length; i++) {
       const doc4 = new jsPDF();
       doc4.addImage('assets/image/certificadoAsesorJaumave.jpg', 'jpg', 0, 0, 210, 300);
-      doc.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 65, 185);
-      doc.text(this.asesorActual.proyecto, 80, 225);
+      doc4.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 70, 185);
+      doc4.text(asesor.proyectos[i], 90, 225);
       doc4.setFontSize(16);
       doc4.setFont('Helvetica');
-      doc4.save("constanciaAsesor.pdf");
+      doc4.save("constancia Asesor "+this.asesorActual.nombres+" Proyecto "+asesor.proyectos[i]+".pdf");
+      }
     break;
     case '6':
+      for (let i = 0; i < asesor.proyectos.length; i++) {
       const doc5 = new jsPDF();
       doc5.addImage('assets/image/certificadoAsesorNuevoLaredo.jpg', 'jpg', 0, 0, 210, 300);
-      doc.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 65, 185);
-      doc.text(this.asesorActual.proyecto, 80, 225);
+      doc5.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 70, 185);
+      doc5.text(asesor.proyectos[i], 90, 225);
       doc5.setFontSize(16);
       doc5.setFont('Helvetica');
-      doc5.save("constanciaAsesor.pdf");
+      doc5.save("constancia Asesor "+this.asesorActual.nombres+" Proyecto "+asesor.proyectos[i]+".pdf");
+      }
     break;
     case '7':
+      for (let i = 0; i < asesor.proyectos.length; i++) {
       const doc6 = new jsPDF();
       doc6.addImage('assets/image/certificadoAsesorVictoria.jpg', 'jpg', 0, 0, 210, 300);
-      doc.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 65, 185);
-      doc.text(this.asesorActual.proyecto, 80, 225  );
-      doc5.setFontSize(16);
-      doc5.setFont('Helvetica');
-      doc5.save("constanciaAsesor.pdf");
+      doc6.text(this.asesorActual.nombres+ " "+ this.asesorActual.a_paterno + " " + this.asesorActual.a_materno , 70, 185);
+      doc6.text(asesor.proyectos[i], 90, 225);
+      doc6.setFontSize(16);
+      doc6.setFont('Helvetica');
+      doc6.save("constancia Asesor "+this.asesorActual.nombres+" Proyecto "+asesor.proyectos[i]+".pdf");
+      }
     break;
     default:
       console.log('sede no encontrada');
--- src/app/services/asesores.service.ts
+++ src/app/services/asesores.service.ts
@@ -3,15 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { ServicesConfig } from '../config/services.config';
 import { Observable } from 'rxjs';
 import { Asesores } from '../models/asesores.model';
+import { Session } from '../models/session.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsesoresService {
-
-  constructor( private http: HttpClient, private servicesConfig: ServicesConfig ) { }
+  sessionData: Session;
+  constructor( private http: HttpClient, private servicesConfig: ServicesConfig ) { 
+    this.sessionData = JSON.parse(localStorage.getItem('session'));
+  }
   getAsesores(): Observable<Asesores[]> {
-    return this.http.get<Asesores[]>(this.servicesConfig.APP_ENDPOINT + 'api/asesores/all-projects');
+    return this.http.get<Asesores[]>(this.servicesConfig.APP_ENDPOINT + 'api/asesores/all?id_sedes=' + this.sessionData.id_sedes);
   }
   postAsesor(body: any): Observable<any> {
     return this.http.post( this.servicesConfig.APP_ENDPOINT + 'api/asesores/nuevo', body );
