badc5b34f58850a468479dbf4f55e57a3b83f108
Santia0098 <56708822+Santia0098@users.noreply.github.com>
2020-09-23 22:47:12 -0500

    nuevas constancias jurado y solo firma direc reyno

--- src/app/judges/judges.component.ts
+++ src/app/judges/judges.component.ts
@@ -230,43 +230,56 @@ export class JudgesComponent implements OnInit {
       case 'El mante':
         const doc = new jsPDF();
         doc.addImage('assets/image/ReconocimientoJuradoMante.jpg', 'jpg', 0, 0, 210, 300).setFont('Helvetica').setFontSize(28).setTextColor('#646464');
-        doc.text(this.juezActual.nombre, 50, 185);
+        doc.text(this.juezActual.nombre, 50, 182);
         doc.save("constancia Juez "+this.juezActual.nombre+".pdf");
       break;
 
       case 'Reynosa':
         const doc1 = new jsPDF();
         doc1.addImage('assets/image/ReconocimientoJuradoReynosa.jpg', 'jpg', 0, 0, 210, 300).setFont('Helvetica').setFontSize(28).setTextColor('#646464');
-        doc1.text(this.juezActual.nombre, 50, 185);
+        doc1.text(this.juezActual.nombre, 50, 182);
+        doc1.addImage('assets/image/DirectorReynosa.png', 'png', 140, 226, 36, 20);
         doc1.save("constancia Juez "+this.juezActual.nombre+".pdf");
       break;
 
       case 'Matamoros':
         const doc2 = new jsPDF();
         doc2.addImage('assets/image/ReconocimientoJuradoMatamoros.jpg', 'jpg', 0, 0, 210, 300).setFont('Helvetica').setFontSize(28).setTextColor('#646464');
-        doc2.text(this.juezActual.nombre, 50, 185);
+        doc2.text(this.juezActual.nombre, 50, 182);
         doc2.save("constancia Juez "+this.juezActual.nombre+".pdf");
       break;
 
       case 'Madero':
         const doc3 = new jsPDF();
         doc3.addImage('assets/image/ReconocimientoJuradoMadero.jpg', 'jpg', 0, 0, 210, 300).setFont('Helvetica').setFontSize(28).setTextColor('#646464');
-        doc3.text(this.juezActual.nombre, 50, 185);
+        doc3.text(this.juezActual.nombre, 50, 182);
         doc3.save("constancia Juez "+this.juezActual.nombre+".pdf");
       break;
 
       case 'Jaumave':
         const doc4 = new jsPDF();
         doc4.addImage('assets/image/ReconocimientoJuradoJaumave.jpg', 'jpg', 0, 0, 210, 300).setFont('Helvetica').setFontSize(28).setTextColor('#646464');
-        doc4.text(this.juezActual.nombre, 50, 185);
+        doc4.text(this.juezActual.nombre, 50, 182);
         doc4.save("constancia Juez "+this.juezActual.nombre+".pdf");
       break;
 
       case 'Nuevo Laredo':
         const doc5 = new jsPDF();
         doc5.addImage('assets/image/ReconocimientoJuradoNuevoLaredo.jpg', 'jpg', 0, 0, 210, 300).setFont('Helvetica').setFontSize(28).setTextColor('#646464');
-        doc5.text(this.juezActual.nombre, 50, 185);
+        doc5.text(this.juezActual.nombre, 50, 182);
         doc5.save("constancia Juez "+this.juezActual.nombre+".pdf");
+      break;
+      case 'Victoria':
+        const doc6 = new jsPDF();
+        doc6.addImage('assets/image/ReconocimientoJuradoVictoria.jpg', 'jpg', 0, 0, 210, 300).setFont('Helvetica').setFontSize(28).setTextColor('#646464');
+        doc6.text(this.juezActual.nombre, 50, 182);
+        doc6.save("constancia Juez "+this.juezActual.nombre+".pdf");
+      break;
+      default:
+        Swal.fire({
+          icon: 'error',
+          title: 'No se encontró la sede'
+        });
       break;
     }
   }
